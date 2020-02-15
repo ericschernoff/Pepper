09fc7400b0d8580058ca366f5e7ffc7135c054e3
Eric Chernoff <ericschernoff@gmail.com>
2020-02-15 04:52:12 +0000

    Initial module setup

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Pepper - It's new $module
+
+# SYNOPSIS
+
+    use Pepper;
+
+# DESCRIPTION
+
+Pepper is ...
+
+# LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Eric Chernoff <ericschernoff@gmail.com>
---
+++ lib/Pepper.pm
@@ -0,0 +1,39 @@
+package Pepper;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pepper - It's new $module
+
+=head1 SYNOPSIS
+
+    use Pepper;
+
+=head1 DESCRIPTION
+
+Pepper is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Chernoff.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Chernoff E<lt>ericschernoff@gmail.comE<gt>
+
+=cut
+
