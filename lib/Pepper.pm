c5b7f99b843729ed94c939384b90f7cb25f770f2
Eric Chernoff <ericschernoff@gmail.com>
2020-02-15 05:17:06 +0000

    More skeleton

--- lib/Pepper.pm
+++ lib/Pepper.pm
@@ -1,39 +1,39 @@
 package Pepper;
-use 5.008001;
-use strict;
-use warnings;
 
-our $VERSION = "0.01";
+use Pepper::DB;
+use Pepper::CGIHandler;
 
+sub new {
 
+}
+
+# we need a default method in case our clients call the wrong thing
+our $AUTOLOAD;
+sub AUTOLOAD {
+	my $self = shift;
+	# figure out the method they tried to call
+	my $called_method =  $AUTOLOAD =~ s/.*:://r;
+	
+	# database function?
+	if ($self->{db}->can($called_method)) {
+
+		return $self->{belt}->$called_method(@_);
+		
+	# cgi handler function?
+	} elsif ($self->{cgi_handler}->can($called_method)) {
+		return $self->{cgi_handler}->$called_method(@_);
+	
+	} else { # hard fail with an error message
+		my $message = "ERROR: No '$called_method' method defined for ".$self->{config}{name}.' objects.';
+		$self->{cgi_handler}->send_response( $message, 1 );
+
+	}
+	
+}
+
+# empty destroy for now
+sub DESTROY {
+	my $self = shift;
+}
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-Pepper - It's new $module
-
-=head1 SYNOPSIS
-
-    use Pepper;
-
-=head1 DESCRIPTION
-
-Pepper is ...
-
-=head1 LICENSE
-
-Copyright (C) Eric Chernoff.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-Eric Chernoff E<lt>ericschernoff@gmail.comE<gt>
-
-=cut
-
